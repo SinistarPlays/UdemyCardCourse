9df6e4902c6eab9bc4d8b88e0f77da3e53c89333
SinistarPlays <two.skillz@hotmail.com>
2023-03-02 16:14:52 +1100

    Introducing Card Game Object.cs and setting up card to become Scriptable Object

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] string cardName;
+    [SerializeField] int manaCost;
+    [SerializeField] int attackPower;
+    [SerializeField] int healthPower;
+
+    void Start()
+    {
+
+    }
+}
