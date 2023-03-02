9f7a8cd62c61dced0ab700b8b44c59af8a1c4166
SinistarPlays <two.skillz@hotmail.com>
2023-03-02 15:31:24 +1100

    Card script linked with Text Mesh Pro to display on card

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public string cardName = "";
+    public TMP_Text manaCostText, attackText, healthText;
+
+    public string cardName;
     public int manaCost;
     public int attackPower;
-    public int defencePower;
-
+    public int healthPower;
 
     void Start()
     {
-
+        manaCostText.text = manaCost.ToString();
+        attackText.text = attackPower.ToString();
+        healthText.text = healthPower.ToString();
     }
 }
